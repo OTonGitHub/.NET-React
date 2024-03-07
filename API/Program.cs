ad66f788c4b786b1120617b65a193a6ea72d7aea
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-03-07 23:10:42 +0000

    modify launch settings

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+// creates kestrel
+// reads configuration
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,7 +18,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // Switched Off For Development, See launchSettings.json
 
 app.UseAuthorization();
 
