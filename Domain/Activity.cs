22fe1e4634e89f9701730da4572e424105c77ed0
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-03-08 22:11:29 +0000

    add activity entity/model

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain;
+
+#nullable disable
+
+public class Activity
+{
+    public Guid Id { get; set; } // By Convention for PK (EF)
+    public string Title { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string Venue { get; set; }
+}
