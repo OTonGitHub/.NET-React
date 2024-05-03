b4940031f3a75689cb132f58967a3945de9ff708
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-05-03 11:55:34 +0000

    S1 E14: add activities API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,25 @@
+using API.Controllers;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controller;
+
+public class ActivitiesController(DataContext ctx) : BaseApiController
+{
+    // if not using primary constructor -> ctor assign _this.ctx
+    private readonly DataContext _ctx = ctx;
+
+    [HttpGet] // ~/api/activities
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await _ctx.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")] // ~/api/activities/GUID
+    public async Task<ActionResult<Activity>> GetActivity(Guid id) // must match Root Parameter
+    {
+        return await _ctx.Activities.FindAsync(id);
+    }
+}
