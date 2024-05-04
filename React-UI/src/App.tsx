d926b2404a51c06a222cf02572ee589a5f47e961
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-05-04 00:34:45 +0000

    S3 E21: return first component

--- React-UI/src/App.tsx
+++ React-UI/src/App.tsx
@@ -1,7 +1,17 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  return <h1>The Man In The High Castle</h1>;
+  return (
+    <>
+      <h1>The Man In The High Castle</h1>
+      {React.createElement(
+        'div',
+        { className: 'TEST' },
+        React.createElement('h1', null, 'Compiled.')
+      )}
+    </>
+  );
 }
 
 export default App;
