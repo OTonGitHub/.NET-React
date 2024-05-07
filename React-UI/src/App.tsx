22c748a7a5f38604f14b241418592c59f63769b8
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-05-07 22:23:21 +0000

    S3 E24: add sandbox code to main

--- React-UI/src/App.tsx
+++ React-UI/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import { ducks } from './Sandbox/demo';
 
 function App() {
   return (
+    // returns JSX, cannot return siblings.
     <>
       <h1>The Man In The High Castle</h1>
       {React.createElement(
@@ -10,6 +12,20 @@ function App() {
         { className: 'TEST' },
         React.createElement('h1', null, 'Compiled.')
       )}
+      {console.log('App.tsx')}
+      {/* map: callback function on iter */}
+      {/*ducks.map((duck) => duck.makeSound("APP"))*/}
+      {ducks.map((duck) => (
+        <div key={duck.name}>
+          {/* need key for looping elements in jsx */}
+          <span>{duck.name}</span>
+          {/* call back needed for on click */}
+          {/* Because, requires a function reference, not  a call */}
+          <button onClick={() => duck.makeSound(duck.name + ': ' + 'quack')}>
+            Make Sound
+          </button>
+        </div>
+      ))}
     </>
   );
 }
