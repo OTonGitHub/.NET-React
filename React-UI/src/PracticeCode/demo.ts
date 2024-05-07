fa73aacadb1e81b56464fa968de622994ef22b47
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-05-07 21:35:28 +0000

    S3 E23: try typestrict strict mode

---
+++ React-UI/src/PracticeCode/demo.ts
@@ -0,0 +1,35 @@
+// inference is preferred, instead of defining type.
+
+let data: number | string = 42; // union type.
+data = '43';
+
+console.log(data);
+
+// duck typing.
+
+interface Duck {
+  name: string;
+  numLegs: number;
+  makeSound: (sound: string) => void;
+}
+
+const duck_1: Duck = {
+  name: 'duck_1',
+  numLegs: 2,
+  // :any not allowed in strict mode.
+  // if param accepted, must be used in strict mode.
+  // param types must match interface contract.
+  makeSound: (sound: string) => console.log(sound),
+};
+
+const duck_2: Duck = {
+  name: 'duck_2',
+  numLegs: 2,
+  // makeQuack: () => console.log('quack!'),
+  makeSound: () => console.log('quack!'),
+};
+
+duck_1.makeSound('quick!');
+duck_2.makeSound('test'); // Okay but Not Declared In duck_2 func?
+
+export const ducks = [duck_1, duck_2];
