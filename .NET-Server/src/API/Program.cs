80001d15515ea712da299b2ddfdab6143b70b336
OTonGitHub <81725875+OTonGitHub@users.noreply.github.com>
2024-05-08 02:05:39 +0000

    S3 E27: add CORS policy

--- .NET-Server/src/API/Program.cs
+++ .NET-Server/src/API/Program.cs
@@ -21,7 +21,7 @@ builder.Services.AddCors(opt =>
     opt.AddPolicy("CorsPolicy", policy =>
     {
         policy.AllowAnyHeader().AllowAnyMethod()
-        .WithOrigins("http://127.0.0.1:3000");
+        .WithOrigins("http://127.0.0.1:3000"); // use configuration?
     });
 });
 
